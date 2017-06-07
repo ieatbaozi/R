c9106488d75cdd3a06afc66066b41da14f3403dd
ieatbaozi <wildh0rses@hotmail.com>
2017-06-07 15:29:31 +0700

    Update test R to power meter.R

--- test R to power meter.R
+++ test R to power meter.R
@@ -2,6 +2,8 @@
 #crimedat <- sqlFetch(myconn, "Crime")
 #pundat <- sqlQuery(myconn, "select * from Punishment")
 
+test.db <- function(){
+
 library(RODBC)
 conn <- odbcDriverConnect('Driver={SQL Server};Server=10.17.127.17;Database=MIT;Uid=sqlreader;Pwd=sqlReader')
 conn
@@ -30,10 +32,11 @@ join MIT..PowerMeterMaster (NOLOCK) on MIT..PowerMeterData.PMMID = MIT..PowerMet
 where MIT..PowerMeterMaster.MeterName like 'MDB%' and MIT..PowerMeterData.AddDate between (GETDATE()-14) and (GETDATE()) 
 order by AddDate DESC "
 
-data.mastermeter <- sqlQuery(conn, "select * from MIT..PowerMeterMaster where MeterName like 'MDB%'")
-data.mastermeter <- data.mastermeter[,c(1,3)]
-data.mastermeter$PMMID <- as.factor(data.mastermeter$PMMID)
-data.mastermeter$MeterName <- as.factor(data.mastermeter$MeterName)
+#data.mastermeter <- sqlQuery(conn, "select * from MIT..PowerMeterMaster where MeterName like 'MDB%'")
+#data.mastermeter <- data.mastermeter[,c(1,3)]
+#data.mastermeter$PMMID <- as.factor(data.mastermeter$PMMID)
+#data.mastermeter$MeterName <- as.factor(data.mastermeter$MeterName)
+
 
 #data.test <- sqlQuery(conn, query.1month)
 data.test <- sqlQuery(conn, query.2weeks)
@@ -41,15 +44,37 @@ data.test <- sqlQuery(conn, query.2weeks)
 data.test1 <- data.test[(data.test$PMMID==25),]
 meter7_1 <- data.test1[,c(3,4)]
 rownames(meter7_1) <- NULL
-as.factor(weekdays(meter7_1$AddDate))
+
+library(lubridate)
 meter7_1used <- subset(meter7_1,minute(meter7_1$AddDate)==0)
 rownames(meter7_1used) <- NULL
-abs(diff(meter7_1used$kWh))
+
 temp <- c(0 ,abs(diff(meter7_1used$kWh)))
 meter7_1used$diffkWh <- temp
+meter7_1used$weekday <- as.factor(weekdays(meter7_1used$AddDate))
+
+odbcClose(conn)
+}
+
+test.plot <- function(){
+
+library(ggplot2)
+library(scales)
+#meter7_1used_temp <- meter7_1used[(meter7_1used$diffkWh > 70),]
+#meter7_1used_temp <- meter7_1used_temp[(meter7_1used_temp$diffkWh <800 ),]
+p <- ggplot(data = meter7_1used, aes(x=meter7_1used$AddDate,y=meter7_1used$diffkWh))
+
+#xmin <- meter7_1used$AddDate
+#xmax <- meter7_1used$AddDate + days(1)
+#ymin <- 0
+#ymax <- 600
+
+
+rects <- data.frame(xstart = as.POSIXct('2017-06-04 00:00:00'), xend = as.POSIXct('2017-06-5 00:00:00'))
+#geom_rect(data = meter7_1used, aes(xmin = rects$xstart, xmax = rects$xend, ymin = -Inf, ymax = Inf), alpha = 0.4)
+}
+p + scale_x_datetime(labels = date_format("%m-%d"), breaks = date_breaks("days")) + theme(axis.text.x = element_text(angle = 45)) + geom_rect(data = meter7_1used, aes(xmin = rects$xstart, xmax = rects$xend, ymin = -Inf, ymax = Inf), alpha = 0.4) + geom_line()
 
 
 
-meter7_1used_temp <- meter7_1used[(meter7_1used$diffkWh > 70),]
-meter7_1used_temp <- meter7_1used_temp[(meter7_1used_temp$diffkWh <800 ),]
-plot(x=meter7_1used_temp$AddDate,y=meter7_1used_temp$diffkWh)
+ggplot(meter7_1used, aes(x=meter7_1used$AddDate,y=meter7_1used$diffkWh, color=ifelse(((meter7_1used$diffkWh>520)),"bad", "good"))) + scale_color_manual(guide=FALSE, values=c("red", "black")) + geom_point()
